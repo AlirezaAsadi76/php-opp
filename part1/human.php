9a86dd526f9d3ce3eafe824f3aaea25c5d57f726
AliReza_Nim <prince137667@gmail.com>
2022-01-14 11:27:32 +0330

    difne set in class

--- part1/human.php
+++ part1/human.php
@@ -3,5 +3,21 @@ class human{
    private $name;
    private $lastname;
    private $code;
+   public function setname($name)
+   {
+       $this->name=$name;
+   }
+   public function setlastname($lastname)
+   {
+       $this->lastname=$lastname;
+   }
+   public function setcode($code)
+   {
+       $this->code=$code;
+   }
 }
 $hu=new human();
+$hu->setname("alireza");
+$hu->setlastname("asadi");
+$hu->setcode("12345");
+var_dump($hu);
